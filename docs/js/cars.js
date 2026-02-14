54df75f093cd0d9659be8582084eaec23cdf8b3d
Dominik Szewczyk <112881814+DominikSzewczyk224180@users.noreply.github.com>
2026-02-14 16:30:51 +0100

    Update cars.js

--- docs/js/cars.js
+++ docs/js/cars.js
@@ -41,33 +41,31 @@ function displayCarsOnMainPage(cars) {
             ${car.images && car.images.length > 0 ? `
                 <img src="${car.images[0]}" alt="${car.brand} ${car.model}" class="car-image">
             ` : `
-                <div class="car-image-placeholder">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#C94A40" viewBox="0 0 24 24">
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-                    </svg>
+                <div class="car-image-placeholder" style="background: var(--bg-light); display: flex; align-items: center; justify-content: center; height: 240px; color: var(--text-light);">
+                    <span style="font-size: 1.2rem;">Brak zdjęcia</span>
                 </div>
             `}
             <div class="car-content">
                 <h3 class="car-title">${car.brand} ${car.model}</h3>
                 <div class="car-details">
                     <span class="car-detail">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.5-13v5.5l4.5 2.7-.8 1.3L7 10V3h1.5z"/>
+                        <svg width="16" height="16" viewBox="0 0 24 24">
+                            <use href="#icon-calendar"/>
                         </svg>
                         ${car.year}
                     </span>
                     ${car.mileage ? `
                         <span class="car-detail">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M12.5 2.5a1 1 0 0 0-1-1h-10a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-11z"/>
+                            <svg width="16" height="16" viewBox="0 0 24 24">
+                                <use href="#icon-road"/>
                             </svg>
                             ${formatNumber(car.mileage)} km
                         </span>
                     ` : ''}
                     ${car.fuel_type ? `
                         <span class="car-detail">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-11z"/>
+                            <svg width="16" height="16" viewBox="0 0 24 24">
+                                <use href="#icon-fuel"/>
                             </svg>
                             ${car.fuel_type}
                         </span>
@@ -77,15 +75,15 @@ function displayCarsOnMainPage(cars) {
                 ${car.autoplac_link ? `
                     <a href="${car.autoplac_link}" target="_blank" rel="noopener noreferrer" class="car-link">
                         Zobacz szczegóły
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M5 12h14M12 5l7 7-7 7"/>
                         </svg>
                     </a>
                 ` : `
-                    <a href="tel:${car.contact_phone || '+48123456789'}" class="car-link">
+                    <a href="tel:${car.contact_phone || '799999100'}" class="car-link">
                         Zadzwoń
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328z"/>
+                        <svg width="16" height="16" viewBox="0 0 24 24">
+                            <use href="#icon-phone"/>
                         </svg>
                     </a>
                 `}
