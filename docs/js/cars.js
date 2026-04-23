9385607c07d15d74815adb84b7febf3da48937d3
Dominik Szewczyk <112881814+DominikSzewczyk224180@users.noreply.github.com>
2026-04-23 09:53:37 +0200

    Update cars.js

--- docs/js/cars.js
+++ docs/js/cars.js
@@ -186,7 +186,7 @@ function openCarModal(idx) {
         ` : ''}
         <div class="cm-actions">
             ${car.autoplac_link ? `
-                <a href="${car.autoplac_link}" target="_blank" rel="noopener" class="btn btn-primary">
+                <a href="${API_URL}/go/autoplac?url=${encodeURIComponent(car.autoplac_link)}" target="_blank" rel="noopener" class="btn btn-primary">
                     <span>Zobacz ogłoszenie</span>
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6M15 3h6v6M10 14L21 3"/></svg>
                 </a>
